6bb35d2861c407d0d29f5835db369ad947eb3304
handq <578936807@163.com>
2014-11-10 22:58:14 +0800

    Update Test01.java

--- src/com/handq/demo/Test01.java
+++ src/com/handq/demo/Test01.java
@@ -2,6 +2,6 @@ package com.handq.demo;
 
 public class Test01 {
 	public static void main(String[] args) {
-		System.out.println("22222223333444444444444444");
+		System.out.println("111111111111111122222223333444444444444444");
 	}
 }
