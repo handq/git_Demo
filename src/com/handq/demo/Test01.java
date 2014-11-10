96c1e1ce6fb95c920363a7032929eb5459ccee64
handq <578936807@163.com>
2014-11-10 22:54:22 +0800

    22

--- src/com/handq/demo/Test01.java
+++ src/com/handq/demo/Test01.java
@@ -2,6 +2,6 @@ package com.handq.demo;
 
 public class Test01 {
 	public static void main(String[] args) {
-		System.out.println("2222222333344");
+		System.out.println("22222223333444444444444444");
 	}
 }
