b8882f880e8b2e131c6b3486d4656de92b173cec
handaquan <578936807@163.com>
2015-01-15 23:05:48 +0800

    aa

--- src/com/handq/demo/Demo.java
+++ src/com/handq/demo/Demo.java
@@ -2,6 +2,6 @@ package com.handq.demo;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("---------------------------");
+		System.out.println("------2222---------------------");
 	}
 }
